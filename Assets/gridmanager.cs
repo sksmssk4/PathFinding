99777d436245d9d1fd8c0065c011d805ad2f724b
sksmssk4 <tlvmak7@naver.com>
2017-12-05 05:23:00 +0900

    DrawLine, CreateWall
    pathfinding

---
+++ Assets/gridmanager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridmanager : MonoBehaviour
+{
+    int x_cell=10;
+    int z_cell;
+    int max_tiles;
+
+
+    static Vector3[] corners = { new Vector3(1, 0, 0), new Vector3(0,0,1)};
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < x_cell; i++)
+        {
+            Debug.DrawLine(new Vector3(-5,0,i-5), new Vector3(5, 0, i-5), Color.red, 1000.0f);
+        }
+        for (int i = 0; i < x_cell; i++)
+        {
+            Debug.DrawLine(new Vector3(i-5, 0, -5), new Vector3(i-5, 0,5 ), Color.red, 1000.0f);
+        }
+        /*
+        for (int i = 0; i < x_cell; i++)
+        {
+            drawRect(i, Color.black, Time.deltaTime);
+        }
+        */
+    }
+ 
+ 
+  
+        
+    
+        
+    /*
+    Vector3 locateToCenter(Vector3 pos)
+    {
+        return pos + new Vector3(0.5f, 0, 0.5f);
+    }
+    */
+    int vectorToCell(Vector3 pos)
+    {
+        return ((int)pos.z) * x_cell + (int)pos.x;
+    }
+
+    Vector3 cellToVector(int cellno)
+    {
+        return new Vector3(cellno % x_cell, cellno / x_cell);
+    }
+
+    void drawRect(int cellno, Color c, float duration = 1000.0f)
+    {
+        Vector3 left = cellToVector(cellno);
+        Debug.DrawLine(left, left + corners[0], c, duration);
+        Debug.DrawLine(left, left + corners[1], c, duration);
+        Vector3 right = left + corners[0] + corners[1];
+        Debug.DrawLine(right, right - corners[0], c, duration);
+        Debug.DrawLine(right, right - corners[1], c, duration);
+    }
+    
+  
+   
+
+}
---
+++ Assets/mera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mos = Input.mousePosition;
+            mos.z = Camera.main.farClipPlane;
+            Vector3 dir = Camera.main.ScreenToWorldPoint(mos);
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, dir, out hit, mos.z))
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = hit.point;
+            }
+        }
+    }
+}
